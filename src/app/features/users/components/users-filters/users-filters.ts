475b9ba7d6fb55a81233ff9fc0ad5ac5c986601e
LuisRocca <luis.rocca96@gmail.com>
2026-01-21 18:11:14 -0500

    feat: starting with clean architecture

---
+++ src/app/features/users/components/users-filters/users-filters.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-filters',
+  imports: [],
+  templateUrl: './users-filters.html',
+  styleUrl: './users-filters.css',
+})
+export class UsersFilters {
+
+}
